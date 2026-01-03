10e868c053b77329e7b6e5339df4e6d34392af0b
xsynch <jdstrickland@gmail.com>
2026-01-02 21:02:59 -0500

    first attempt at handling commands

--- app/main.go
+++ app/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
 func main() {
+	fmt.Printf("$ ")
+	var userInput string
+	n, err := fmt.Scanln(&userInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if n > 1{
+		log.Fatal("Please include only one command right now")
+	}
+	fmt.Printf("%s: command not found\n",userInput)
 	
-	fmt.Print("$ ")
 }
