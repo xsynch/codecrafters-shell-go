b3dc91bead015059b3166ac79c9bd45528fc0552
xsynch <jdstrickland@gmail.com>
2026-01-03 14:10:25 -0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,30 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
 
 var _ = fmt.Print
 
 func main() {
-	
+	userExit,_ := regexp.Compile("^exit$")
+	userEcho,_ := regexp.Compile("^echo ")
 	for {
-		fmt.Printf("$ ")
-		var userInput string
-		n, err := fmt.Scanln(&userInput)
+		fmt.Print("$ ")
+		reader := bufio.NewReader((os.Stdin))
+		line, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
+			os.Exit(1)
 		}
-		if n > 1{
-			log.Fatal("Please include only one command right now")
-		}
-		if userInput == "exit"{
+		line = strings.Trim(line,"\n")
+		// fmt.Printf("Line entered: %s\n",strings.Trim(line,"\n"))
+
+		// var userInput string
+		// _, err := fmt.Scanln(&userInput)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		if userExit.MatchString(line){
 			os.Exit(0)
 		}
-		fmt.Printf("%s: command not found\n",userInput)
+		if userEcho.MatchString(line){
+			processEcho(line)
+			continue 
+		}
+		
+		fmt.Printf("%s: command not found\n",line)
 	}
 	
+}
+
+
+func processEcho(input string){
+	data := strings.Split(input, " ")
+	if len(data) == 1{
+		fmt.Println("")
+		return 
+	}
+	if len(data) > 1{
+		fmt.Printf("%s\n",strings.Join(data[1:]," "))
+		return 
+	}
 }
