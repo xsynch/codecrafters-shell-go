100ca9498bbe1ad8b396e4599621b1ca329c84d9
xsynch <jdstrickland@gmail.com>
2026-01-02 21:18:01 -0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,19 +5,22 @@ import (
 	"log"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
+
 var _ = fmt.Print
 
 func main() {
-	fmt.Printf("$ ")
-	var userInput string
-	n, err := fmt.Scanln(&userInput)
-	if err != nil {
-		log.Fatal(err)
+	
+	for {
+		fmt.Printf("$ ")
+		var userInput string
+		n, err := fmt.Scanln(&userInput)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if n > 1{
+			log.Fatal("Please include only one command right now")
+		}
+		fmt.Printf("%s: command not found\n",userInput)
 	}
-	if n > 1{
-		log.Fatal("Please include only one command right now")
-	}
-	fmt.Printf("%s: command not found\n",userInput)
 	
 }
